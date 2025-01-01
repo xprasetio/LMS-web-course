dd13b08b0dd6e0bb2cf42c352dc8ea8125db053f
eko-prasetio <caedablerg@gmail.com>
2025-01-02 00:35:48 +0700

    slicing sign-in

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManagerHome from "../pages/ManagerHome";
+import SignInPage from "../pages/SignIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ManagerHome />,
+  },
+  {
+    path: "/manager/sign-in",
+    element: <SignInPage />,
   },
 ]);
 
